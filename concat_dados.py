c17ca65ab66e688701f77dfde789cb8b5b5c8c5a
PatQuei <160976152+PatQuei@users.noreply.github.com>
2024-08-28 00:32:29 +0000

    Inclusão de readMe e alteração de código

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+apelido = nome + sobrenome
+
+print(apelido)
