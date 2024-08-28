866d50776e68fd4c5e63a26ca09d5dac1e27fd59
PatQuei <160976152+PatQuei@users.noreply.github.com>
2024-08-28 01:00:51 +0000

    alterando código

---
+++ ope_mat.py
@@ -0,0 +1,2 @@
+# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
---
+++ repet_txt.py
@@ -0,0 +1,9 @@
+#Agora vamos solicitar uma string e um numero inteiro como entrada. Depois teremos que retornar a string repetida o numero de vezes informado
+
+# Solicitar uma string
+texto = input("Digite uma palavra ou frase: ")
+
+# Solicitar um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+print ((texto + " ") * numero)
